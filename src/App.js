54fe72f2359f39bb42b785f48da9dc480aec0956
ArmenMel <armen.meliksetyann@gmail.com>
2023-03-29 16:06:53 +0400

    Header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
+import { TextField } from '@mui/material';
 import './App.css';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      Alias by Shirakatsy
+      <Header/>
+      <TextField id="standard-basic" label="Team 1 Name" variant="standard" />
+      <TextField id="standard-basic" label="Team 2 Name" variant="standard" />
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header(){
+    return(
+        <div className="header">
+            <h1>Alias</h1>
+            <img src='https://www.drupal.org/files/issues/2022-09-16/qmark-400x684new.png'/>
+        </div>
+    )
+}
+
+export default Header
